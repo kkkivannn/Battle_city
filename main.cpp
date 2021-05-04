5889fe8fb80b349a6fbd316ec5da7ba8ce886f1d
Ivan <ikiselev288@gmail.com>
2021-05-05 00:08:08 +0300

    Дбавил 4-ую строчку кода

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ int main()
 	std::cout <<"hello world!"<<std::endl;
 	std::cout <<"Second line"<<std::endl;
 	std::cout <<"Therd line"<<std::endl;
+	std::cout <<"Hello lox!"<<std::endl;
 	return 0;
 }
 
