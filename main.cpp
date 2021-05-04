7d644b8fb7b4048ee690ebd62298687d6158e65b
Ivan <ikiselev288@gmail.com>
2021-05-05 00:14:03 +0300

    Added fourth line

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ int main()
 	std::cout <<"hello world!"<<std::endl;
 	std::cout <<"Second line"<<std::endl;
 	std::cout <<"Therd line"<<std::endl;
-	std::cout <<"Hello lox!"<<std::endl;
+	std::cout <<"hello lox!"<<std::endl;
 	return 0;
 }
 
